6914634440942df4c51ac38c2b02457f6a170835
Győry Botond <gyory.botond@verebelyszki.hu>
2023-01-08 20:25:13 +0100

    Beadandó

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
     internal class Program
     {
         static int[] kocka = new int[5];
+        static int Osszes = 0;
         static Random rnd = new Random();
         static void Main(string[] args)
         {
@@ -12,7 +13,8 @@
 
         private static void pontszam()
         {
-            int HaromUgyanolyan = 0;
+            bool Par = false;
+            bool HaromUgyanolyan = false;
             int Egyesek = 0;
             int Kettesek = 0;
             int Harmasok = 0;
@@ -47,78 +49,95 @@
                     Otosok++;
                 }
                 //hatosok
-                if(kocka[i] == 6)
+                if (kocka[i] == 6)
                 {
                     Hatosok++;
-                }   
+                }
             }
             Console.WriteLine($"egyesek: {Egyesek}");
             Console.WriteLine($"kettesek: {Kettesek * 2}");
             Console.WriteLine($"Harmasok: {Harmasok * 3}");
             Console.WriteLine($"Negyesek: {Negyesek * 4}");
-            Console.WriteLine($"Otosok: {Otosok * 5}"); 
+            Console.WriteLine($"Otosok: {Otosok * 5}");
             Console.WriteLine($"Hatosok: {Hatosok * 6}");
+            //párok
+            if (Egyesek == 2)
+            {
+                Par = true;
+            }
+            if (Kettesek == 2)
+            {
+                Par = true;
+            }
+            if (Harmasok == 2)
+            {
+                Par = true;
+            }
+            if (Negyesek == 2)
+            {
+                Par = true;
+            }
+            if (Otosok == 2)
+            { 
+                Par = true;
+            }
+            if (Hatosok == 2)
+            {
+                Par = true;
+            }
+
             //három ugyanolyan
-            if (Egyesek >= 3)
+            if (Egyesek == 3 || Kettesek == 3 || Harmasok == 3 || Negyesek == 3 || Otosok == 3 || Hatosok == 3)
             {
-                Console.WriteLine($"Három ugyanolyan: 3");
+                Console.WriteLine($"Három ugyanolyan: {Osszes}");
+                HaromUgyanolyan = true;
             }
-            if (Kettesek >= 3)
-            {
-                Console.WriteLine($"Három ugyanolyan: 6");
-            }
-            if (Harmasok >= 3)
-            {
-                Console.WriteLine($"Három ugyanolyan: 9");
-            }
-            if (Negyesek >= 3)
-            {
-                Console.WriteLine($"Három ugyanolyan: 12");
-            }
-            if (Otosok >= 3)
-            {
-                Console.WriteLine($"Három ugyanolyan: 15");
-            }
-            if (Hatosok >= 3)
-            {
-                Console.WriteLine($"Három ugyanolyan: 18");
-            }
+
             //négy ugyanolyan
-            if (Egyesek >= 4)
+            if (Egyesek == 4 || Kettesek == 4 || Harmasok == 4 || Negyesek == 4 || Otosok == 4 || Hatosok == 4)
             {
-                Console.WriteLine($"Négy ugyanolyan: 4");
+                 Console.WriteLine($"Négy ugyanolyan: {Osszes}");
             }
-            if (Kettesek >= 4)
+
+            //fullhouse
+            if(Par==true && HaromUgyanolyan == true)
             {
-                Console.WriteLine($"Négy ugyanolyan: 8");
+                Console.WriteLine("Full house: 25");
             }
-            if (Harmasok >= 4)
+
+            //small straight
+            if(Egyesek>=1 && Kettesek>=1 && Harmasok >= 1 && Negyesek>=1 || Kettesek >= 1 && Harmasok >= 1 && Negyesek >= 1 && Otosok >= 1 || Harmasok >= 1 && Negyesek >= 1 && Otosok >= 1 && Hatosok >= 1)
             {
-                Console.WriteLine($"Négy ugyanolyan: 12");
+                Console.WriteLine("Small straight: 30");
             }
-            if (Negyesek >= 4)
+
+            //largehouse
+            if (Egyesek >= 1 && Kettesek >= 1 && Harmasok >= 1 && Negyesek >= 1 && Otosok >= 1 || Kettesek >= 1 && Harmasok >= 1 && Negyesek >= 1 && Otosok >= 1 && Hatosok >= 1)
             {
-                Console.WriteLine($"Négy ugyanolyan: 16");
+                Console.WriteLine("Large house: 40");
             }
-            if (Otosok >= 4)
+
+            //chance
+            Console.WriteLine($"Chance: {Osszes}");
+
+            //YAHTZEE
+            if (Egyesek == 5 || Kettesek == 5 || Harmasok == 5 || Negyesek == 5 || Otosok == 5 || Hatosok == 5)
             {
-                Console.WriteLine($"Négy ugyanolyan: 20");
+                Console.WriteLine("YAHTZEE: 50");
             }
-            if (Hatosok >= 4)
-            {
-                Console.WriteLine($"Négy ugyanolyan: 24");
-            }
+
 
         }
 
         private static void dobas()
         {
-            
-            for(int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++)
             {
-                kocka[i] = rnd.Next(1,7);
+                kocka[i] = rnd.Next(1, 7);
+                Osszes += kocka[i];
                 Console.WriteLine(kocka[i]);
             }
         }
-    }
+        
+    } 
 }
